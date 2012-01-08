f2c9d2d61debedada3805740cb8813001f70a652
Chris Petersen <geek+github@ex-nerd.com>
2012-01-07 23:29:42 -0800

    version bump for documentation

--- prep.py
+++ prep.py
@@ -5,7 +5,7 @@ prep
 Main callable module
 """
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 import os, re, sys, time, socket
 import subprocess
--- setup.py
+++ setup.py
@@ -14,8 +14,8 @@ setup_args = dict(
     author           = 'Chris Petersen',
     author_email     = 'geek@ex-nerd.com',
     url              = 'https://github.com/ex-nerd/prep',
-    license          = 'LICENSE.txt',
-    description      = 'Pre-deployment config and template parser',
+    license          = 'Modified BSD',
+    description      = 'Pre-deployment configuration parser/generator',
     long_description = open('README.rst').read(),
     install_requires = install_requires,
     py_modules       = ['prep'],
@@ -23,7 +23,17 @@ setup_args = dict(
         'console_scripts': [
             'prep = prep:prep',
         ],
-    }
+    },
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Environment :: Console",
+        "Topic :: Utilities",
+    ],
 )
 
 if __name__ == '__main__':
