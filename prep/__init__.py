00b4ba13492d30112409d602ce1e307d32fba66b
Chris Petersen <geek+github@ex-nerd.com>
2012-01-07 16:29:15 -0800

    Better argument parsing, start templates
    Nothing actually works yet.

--- prep/__init__.py
+++ prep/__init__.py
@@ -13,25 +13,35 @@ import ConfigParser
 import argparse
 
 _conf_file = '.prep'
-_conf_sections = ['prep', 'pre', 'post', 'files', 'includes', 'vars']
+_conf_sections = ['prep', 'pre', 'post', 'files', 'vars']
 
 def prep():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-m', '--mode', nargs='?', help='Preparation mode')
+    parser.add_argument('-m', '--mode', nargs='?', help='Preparation mode (can also specify as --MODE)')
     parser.add_argument('-H', '--host', default=socket.gethostname(), nargs='?', help='Hostname to prepare for')
     parser.add_argument('path', default='.', nargs='?', type=_arg_is_dir, help="Path to prepare (default .)")
     (args, unknown_args) = parser.parse_known_args()
     # Attempt "smart" mode detection when no explicit mode has been provided
-    if not args.mode:
-        for arg in unknown_args:
-            if arg.startswith('--') and '=' not in arg:
-                unknown_args.remove(arg)
-                args.mode = arg[2:]
-                break
+    for arg in unknown_args[:]:
+        if not arg.startswith('--'):
+            continue
+        # Arbitrary conf override argument
+        if '=' in arg:
+            (var,val) = arg[2:].split('=')
+            setattr(args, var, val)
+            unknown_args.remove(arg)
+        # single --arg shortcut can be used instead of --mode=arg
+        elif not args.mode:
+            args.mode = arg[2:]
+            unknown_args.remove(arg)
     #print args
+    #print vars(args).items()
     #print unknown_args
     #print sys.argv
     #print "-------------------"
+    # Need a mode
+    if not args.mode:
+        parser.error("Please specify --mode=MODE or --MODE")
     # Walk the specified path
     path = os.path.abspath(args.path)
     for (dirpath, dirnames, filenames) in os.walk(path):
@@ -40,23 +50,21 @@ def prep():
         conf = _load_conf(dirpath, args)
         _do_prep(dirpath, conf)
 
-def _arg_is_dir(path):
-    """argparse "type" function to validate a directory path"""
-    if os.path.isdir(path):
-        return path
-    raise argparse.ArgumentTypeError(msg = "{0} is not a directory".format(path))
-
 def _do_prep(dirpath, conf):
-    print conf
+    #print conf
     os.chdir(dirpath)
     # Process the pre-prep tasks
     _do_pre_post('pre', conf['pre'])
     # Process variables
-    pass
+    vars = dict(conf['vars'])
     # Process includes
-    pass
+    #inc = {}
+    #for pair in conf['includes']:
+    #    (name, file) = pair
+    #    inc[name] = open(file, 'r').read().strip()
     # Process files
-    pass
+    for pair in conf['files']:
+        (src, dest) = pair
     # Process the post-prep tasks
     _do_pre_post('post', conf['post'])
 
@@ -65,7 +73,7 @@ def _do_pre_post(which, items):
     Currently only supports the "run" command.
     @todo Standardize sequential commands like run.2
     @todo natural sort sequential commands (i.e. run.2 < run.10)
-    @todo more macros: chmod, mkdir, ???
+    @todo more macros: chmod, mkdir, create-file, ???
     """
     for item in items:
         (k, v) = item
@@ -73,6 +81,26 @@ def _do_pre_post(which, items):
             _run_commands(v)
         else:
             raise ValueError('Unrecognized "{0}" command "{1}"'.format(which, k))
+
+def _process_file(src, dest, vars):
+    t = SimpleTemplate(src, vars)
+    data = open(src, 'r').read()
+    # Process includes
+    data = re.sub(r'##inc:([\w\.\-]+)##', replace_inc, data)
+    # Process basic logic
+    ifpat = re.compile(r'##if:(\S+)## *\n?(.+?) *##endif## *\n?', re.S)
+    while True:
+        (data, n) = re.subn(ifpat, replace_if, data)
+        if not n:
+            break
+    # Process variables
+    varpat = re.compile(r'##([\w\.\-]+)##')
+    while True:
+        (data, n) = re.subn(varpat, replace_var, data)
+        if not n:
+            break
+    # Output
+    open(dest, 'w').write(data)
 
 def _run_commands(commands):
     """Run one or more shell commands"""
@@ -150,11 +178,91 @@ def _load_conf(dirpath, args):
                     conf[key][i] = (item[0], eval(val))
                 elif val.title() in ('True', 'False', 'None'):
                     conf[key][i] = (item[0], eval(val.title()))
+    # Override conf settings with CLI argument values, and provide some
+    # additional values
+    conf['vars'] = _smart_merge(
+        conf['vars'],
+        filter(lambda x: x[0] not in ('path'), vars(args).items()) + [
+            ('root', dirpath),
+            ('user', os.environ['USER']),
+            ('time', str(int(time.time()))),
+        ]
+        )
     # Return
     return conf
 
-def _init():
-    pass
+#####################################
+# Template handlers
+
+class Template(object):
+    def __init__(self, file, vars):
+        if isinstance(vars, dict):
+            self.vars = vars
+        else:
+            self.vars = dict(vars)
+
+class SimpleTemplate(Template):
+    def render(self, file):
+        # Define the replacement functions
+        # Note:  We want these to raise exceptions if things go wrong, so don't
+        #        be overzealous in trapping them.
+        def repl_include(m):
+            file = m.group(1)
+            # extract the file
+            #return self.render(file)
+            pass
+        def repl_if(m):
+            # Yes, we want this to raise an exception
+            if '!=' in m.group(1):
+                (var, val) = m.group(1).split('!=')
+                neg = True
+            else:
+                (var, val) = m.group(1).split('=')
+                neg = False
+            if ',' in val:
+                val = val.split(',')
+            else:
+                val = [val]
+            if neg and vars[var] not in val:
+                return m.group(2)
+            if not neg and vars[var] in val:
+                return m.group(2)
+            return ''
+        def repl_var(m):
+            return self.vars[m.group(1)]
+
+        # Load the file
+        data = open(file, 'r').read()
+        # Process includes
+        data = re.sub(r'##inc:([\w\.\-]+)##', replace_inc, data)
+        # Process basic logic
+        ifpat = re.compile(r'##if:(\S+)## *\n?(.+?) *##endif## *\n?', re.S)
+        while True:
+            (data, n) = re.subn(ifpat, replace_if, data)
+            if not n:
+                break
+        # Process variables
+        varpat = re.compile(r'##([\w\.\-]+)##')
+        while True:
+            (data, n) = re.subn(varpat, replace_var, data)
+            if not n:
+                break
+        # Output
+        outfile = file[:-3]
+        open('{0}/{1}'.format(cdir, outfile), 'w').write(data)
+        pass
+
+#####################################
+# Utility methods
+
+def _arg_is_dir(path):
+    """argparse "type" function to validate a directory path"""
+    if os.path.isdir(path):
+        return path
+    raise argparse.ArgumentTypeError(msg = "{0} is not a directory".format(path))
+
+#####################################
+# Main
 
 if __name__ == '__main__':
     prep()
